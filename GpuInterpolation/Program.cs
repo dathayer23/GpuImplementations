767393691cc1fefefbc8268dca7e2ec1f32e7de9
dathayer23 <dathayer23@gmail.com>
2015-04-17 16:47:26 -0700

    Add other available report ids

--- GpuInterpolation/Program.cs
+++ GpuInterpolation/Program.cs
@@ -24,7 +24,7 @@ namespace GpuInterpolation
 {
 	class Program
 	{
-
+        //1862, 649, 1852, 648, 1859, 1854, 1853, 1861, 1855, 1858, 1857, 1856, 1863, 1860
         const string dbConnectionString = @"server=127.0.0.1;port=5432;database=RisingTide;user id=postgres;password=Aditi01*;enlist=true;pooling=false;minpoolsize=1;maxpoolsize=100;timeout=150;";// databaseName="RisingTide" isDefault="true"
         static IControllerDataService _dbService; 
 		static void Main(string[] args)
@@ -32,7 +32,7 @@ namespace GpuInterpolation
             _dbService = new ControllerDataService(dbConnectionString);
             var interpolater = new CalculateHeatmap();
             Console.WriteLine("Retrieve Yield Report Data id = {0} and Boundary data from Database", 1862);
-            YieldReportData yieldData = _dbService.GetYieldReport(1862, false, false);
+            YieldReportData yieldData = _dbService.GetYieldReport(649, false, false);
             var boundary = _dbService.GetFieldBoundary(yieldData.FieldId);
             //var data = yieldData.YieldData.Yield.data;
             Console.WriteLine("Call GPU interpolation function");
